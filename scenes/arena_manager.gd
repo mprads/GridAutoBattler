9a7b9bfc52f49d8c2e45b4211fdb983c9eb8ed7c
Myles Rademaker <mylesprademaker@gmail.com>
2023-12-11 22:13:09 -0800

    Party is now rendered on player tile map on load

--- scenes/arena_manager.gd
+++ scenes/arena_manager.gd
@@ -1,18 +1,42 @@
 extends Node2D
 
-@export var unit_object: PackedScene
+@export var unit_scene: PackedScene
 
 @onready var tile_controller = $"../TileController"
 
-var party: Array[Unit] = []
+var resource_party: Array[Unit] = []
+var party: Array[Node2D] = []
+var selected_unit: Node2D
 
 func _ready() -> void:
 	#party = Party.get_party()
-	party.push_back(UnitPool.get_random())
+	resource_party = UnitPool.get_all()
 
-	for unit in party:
-		var unit_instance = unit_object.instantiate()
+	_generate_party()
+
+	selected_unit = party[0]
+
+func _process(delta: float) -> void:
+	if selected_unit:
+		pass
+	pass
+
+
+func _generate_party():
+	var count = 0
+	for unit in resource_party:
+		var unit_instance = _generate_unit(unit)
 		var player_party = get_tree().get_first_node_in_group("PlayerParty")
-		unit_instance.set_unit_type(unit)
+		unit_instance.global_position = tile_controller.convert_id_to_world_position(Vector2i(0, count), "player")
+		tile_controller.update_tile_data(Vector2i(0, count), "unit", "player", unit_instance)
+
 		player_party.add_child(unit_instance)
-		unit_instance.global_position = Vector2(58, 87)
+		party.push_back(unit_instance)
+		count += 1
+
+
+func _generate_unit(unit: Unit) -> Node2D:
+	var unit_instance = unit_scene.instantiate()
+	unit_instance.set_unit_type(unit)
+	return unit_instance
+
--- scenes/controllers/tile_controller.gd
+++ scenes/controllers/tile_controller.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@export var player_tile_map: TileMap
-@export var enemy_tile_map: TileMap
+@onready var player_tile_map: TileMap = $"../PlayerTileMap"
+@onready var enemy_tile_map: TileMap = $"../EnemyTileMap"
 
 const DIRECTIONS = [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.UP, Vector2i.DOWN]
 
@@ -9,19 +9,47 @@ var ground_source = 0
 var ground_layer = 0
 var ground_atlas = Vector2i(0, 0)
 
-var player_grid = {}
-var enemy_grid = {}
+
+var tile_maps: Dictionary
+var grids: Dictionary = {
+		"player": {},
+		"enemy": {}
+	}
 
 
 func _ready() -> void:
-	generate_map(4, 4, player_tile_map, player_grid)
-	generate_map(4, 4, enemy_tile_map, enemy_grid)
+	tile_maps = {
+		"player": player_tile_map,
+		"enemy": enemy_tile_map
+	}
+
+	_generate_map(4, 4, player_tile_map, grids["player"])
+	_generate_map(4, 4, enemy_tile_map, grids["enemy"])
 
 
-func generate_map(x: int, y: int, map, grid) -> void:
+func _generate_map(x: int, y: int, map, grid) -> void:
 	for i in x:
 		for j in y:
 			grid[str(Vector2i(i, j))] = {
-				"occupied": false
+				"unit": null
 			}
 			map.set_cell(ground_layer, Vector2i(i, j), ground_source, ground_atlas)
+
+
+func convert_world_position_to_id(pos: Vector2, tile_map: String) -> Vector2i:
+	if tile_map == null:
+		return Vector2i(0,0)
+
+	return tile_maps[tile_map].local_to_map(pos)
+
+
+func convert_id_to_world_position(id: Vector2i, tile_map: String) -> Vector2:
+	if tile_map == null:
+		return Vector2(0,0)
+
+	#Need to account for the displacement of the tilemap and half of a tile
+	return tile_maps[tile_map].global_position + tile_maps[tile_map].map_to_local(id) - Vector2(0, 16)
+
+
+func update_tile_data(id: Vector2i, key: String, grid: String, value ) -> void:
+	grids[grid][str(id)][key] = value
